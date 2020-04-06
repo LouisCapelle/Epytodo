6ab074b0c01e845801bc44cd9e0c1e3f937e88dc
louiscapelle <louis.capelle@epitech.eu>
2020-04-06 19:32:42 +0200

    corrected html and added routes in views

--- epytodo/views.py
+++ epytodo/views.py
@@ -16,6 +16,14 @@ connect = sql.connect(host='localhost', unix_socket='/var/run/mysqld/mysqld.sock
 def index():
     return render_template("templates.html")
 
+@app.route('/to_do', methods=['GET'])
+def to_do():
+    return render_template("to_do.html")
+
+@app.route('/home', methods=['GET'])
+def home():
+    return render_template("home.html")
+
 @app.route('/add_user', methods=['GET'])
 def add_user():
     return render_template("add_user.html")
