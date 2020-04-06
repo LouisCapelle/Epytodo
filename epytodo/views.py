08862c2392c298b312d9840c8efd2c1db53c18a6
louiscapelle <louis.capelle@epitech.eu>
2020-04-06 14:30:56 +0200

    navigation updated

--- epytodo/views.py
+++ epytodo/views.py
@@ -12,5 +12,13 @@ app = Flask(__name__)
 app.config.from_object('config')
 connect = sql.connect(host='localhost', unix_socket='/var/run/mysqld/mysqld.sock', user='root', passwd='root', db='epytodo')
 
+@app.route('/', methods=['GET'])
+def index():
+    return render_template("templates.html")
+
+@app.route('/add_user', methods=['GET'])
+def add_user():
+    return render_template("add_user.html")
+
 if __name__ == "__main__":
     app.run()
